c397d9d8d6fb7b9c510066c683c27c020ba8789d
Aki <akilesh.praveen@gmail.com>
2024-08-29 00:05:07 -0700

    feat: add goto preview

--- init.lua
+++ init.lua
@@ -195,7 +195,31 @@ require("lazy").setup({
       config = function ()
         require'alpha'.setup(require'alpha.themes.startify'.config)
       end
-    };
+    },
+    -- GOTO previews
+    {
+      "rmagatti/goto-preview",
+      config = function()
+        require('goto-preview').setup {
+          width = 120; -- Width of the floating window
+          height = 25; -- Height of the floating window
+          border = {"╭", "─" ,"╮", "│", "╯", "─", "╰", "│"}; -- Border characters of the floating window
+          default_mappings = false; -- Bind default mappings
+          debug = false; -- Print debug information
+          opacity = 10; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+          resizing_mappings = false; -- Binds arrow keys to resizing the floating window.
+          post_open_hook = nil; -- A function taking two arguments, a buffer and a window to be ran as a hook.
+          references = { -- Configure the telescope UI for slowing the references cycling window.
+            telescope = require("telescope.themes").get_dropdown({ hide_preview = false })
+          };
+          -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
+          focus_on_open = true; -- Focus the floating window when opening it.
+          dismiss_on_move = false; -- Dismiss the floating window when moving the cursor.
+          force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
+          bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
+        }
+      end
+    }
   },
   install = { colorscheme = { "nightfox" } },  
   checker = { enabled = true },
@@ -241,8 +265,7 @@ for _, server in ipairs(servers) do
   lspconfig[server].setup({})
 end
 
--- Keybindings
--- Keybindings
+-- GOTO defn
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -261,5 +284,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 
--- START SCREEN
+-- GOTO References
+vim.api.nvim_set_keymap("n", "gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", {noremap=true, silent=true})
 
