e2929bd11fdaeecdb24e1980ff52a220c94a955c
Aki <akilesh.praveen@gmail.com>
2024-10-05 00:50:28 -0700

    feat: start cognito auth

--- init.lua
+++ init.lua
@@ -480,11 +480,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
     local opts = { buffer = ev.buf }
-    -- Modified gd keybinding to open in new tab
-    vim.keymap.set('n', 'gd', function()
-      vim.cmd('tab split')
-      vim.lsp.buf.definition()
-    end, opts)
+    -- Default gd keybinding
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 
     -- Keep other keybindings as they are
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
